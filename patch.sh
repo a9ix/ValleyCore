b77b422b9ae1e441c292b05bb6b6275510932133
a9ix <neonItem+2g@protonmail.com>
2025-09-28 16:39:03 +0300

    Create build workflow

---
+++ patch.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${CURRENT_DIR}"
+
+# the architecture of PE executables and libraries is stored at 0x84
+# we need to change it from 8664 to AA64
+# it's little endian, so the byte to change is at 0x85
+arch_pos=$((16#85))
+
+filesfound=0
+
+arch_as_hex() {
+    # returns the upper arch byte as hex from file $1
+    od -A n -j $arch_pos -N 1 -t x1 "$1" | grep -Eo "[0-9a-f]+" | tr -d "\n"
+}
+
+patch_dll() {
+    printf "\xaa" | dd of="$1" bs=1 seek=$arch_pos conv=notrunc
+}
+
+patchifvalid() {
+    if ! [ -a "$1" ]; then
+        echo "$1 does not exist in this directory"
+        return
+    fi
+
+    filesfound=1
+    arch=$(arch_as_hex "$1")
+    if [ $arch = "86" ]; then
+        patch_dll "$1"
+    elif [ $arch = "aa" ]; then
+        echo "$1 seems to be already patched"
+    else
+        echo "$1 may be malformed"
+    fi
+}
+
+warn() {
+    echo "No files could be patched. Make sure the patch.sh script is in the same directory as the dll files."
+}
+
+# no unmanaged code is contained in these, so this works
+patchifvalid "Stardew Valley.dll"
+patchifvalid "MonoGame.Framework.dll"
+patchifvalid "xTile.dll"
+patchifvalid "StardewValley.GameData.dll"
+patchifvalid "BmFont.dll"
+patchifvalid "Lidgren.Network.dll"
+
+if ! [ $filesfound -eq 1 ]; then
+    warn
+fi
